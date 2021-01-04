dc52ffa51bffffca7bf10d9b9dfd97f0542699b5
olenmg <o.olenmg@gmail.com>
2021-01-04 21:43:57 +0900

    [Modify] Knapsack algorithm (add time complexity)

--- algorithm-set/other/knapsack-algorithm.cpp
+++ algorithm-set/other/knapsack-algorithm.cpp
@@ -8,15 +8,19 @@
     For solving backpack problem. This algorithm is based on DP.
     dp[i][j] means max value of backpack with usable weight of w when only 1 ~ i-th item can be stored.
     dp[i][j] = max(with i-th item, without i-th item) 
-
+    Time complexity: O(NW) (N : # of stuffs, W : usable weight)
+    
     배낭 문제 해결을 위한 DP 기반 알고리즘.
     dp[i][j]는 i번째 물건까지 넣을 수 있일 때 가용무게 j인 배낭의 최대 가치.
     dp[i][j] = max(i번째 물건을 넣을 때, i번째 물건을 넣지 않을 때) 로 구현된다.
+    시간 복잡도: O(NW) (N : 물건 개수, W : 가용 무게)
+    
     헷갈렸던 점은 dp[i][j - 1]은 고려하지 않아도 되는가였는데, 
     i번째 물건을 넣지 않는 경우는 가용무게 j - 1 < j이므로 당연히 dp[i - 1][j]가 당연히 더 클 것이고,
     i번째 물건을 넣는 경우도 가용무게가 j - 1 < 이므로 dp[i][j - 1] < dp[i - 1][j - w[i]] + v[i] 임이 자명하다. 
 
     특별한건 없는데 가끔 헷갈릴 때가 있어서 넣음.
+    
 */
 #include <iostream>
 using namespace std;
